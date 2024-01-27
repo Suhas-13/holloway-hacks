b8072e7bdabe1477b8a5048497d379f69dec595b
Ollie Stone <ollieandy.stone@gmail.com>
2024-01-27 18:09:58 +0000

    make websocket resilient

--- extension/background.js
+++ extension/background.js
@@ -1,51 +1,70 @@
-let webSocket = new WebSocket("ws://localhost:8001");
+let running = false;
 
-webSocket.onopen = (event) => {
-    console.log("websocket open");
-};
+function create_web_socket() {
+    if (running) return;
 
-function chunkString(str, length) {
-    return str.match(new RegExp("(.|[\r\n]){1," + length + "}", "g"));
-}
+    let webSocket = new WebSocket("ws://localhost:8001");
+    running = true;
 
-webSocket.onmessage = (event) => {
-    console.log("data requested");
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (tabs.length == 0) {
-            return;
-        }
+    webSocket.onopen = (event) => {
+        console.log("websocket open");
+    };
 
-        let id = tabs[0].id;
-        let url = tabs[0].url;
-        let title = tabs[0].title;
+    function chunkString(str, length) {
+        return str.match(new RegExp("(.|[\r\n]){1," + length + "}", "g"));
+    }
 
-        if (url.startsWith("chrome://")) {
-            webSocket.send("");
-            return;
-        }
-
-        chrome.tabs.executeScript(
-            id,
-            {
-                code: "document.all[0].innerText",
-                runAt: "document_start",
-            },
-            function (results) {
-                if (results[0].length == 0) {
-                    webSocket.send("pdf:" + url);
-                } else {
-                    webSocket.send("text:start:" + url);
-                    chunkString(results[0], 500).forEach((chunk) => {
-                        webSocket.send(chunk);
-                    });
-                    webSocket.send("text:end");
+    webSocket.onmessage = (event) => {
+        console.log("data requested");
+        chrome.tabs.query(
+            { active: true, currentWindow: true },
+            function (tabs) {
+                if (tabs.length == 0) {
+                    return;
                 }
+
+                let id = tabs[0].id;
+                let url = tabs[0].url;
+                let title = tabs[0].title;
+
+                if (url.startsWith("chrome://")) {
+                    webSocket.send("");
+                    return;
+                }
+
+                chrome.tabs.executeScript(
+                    id,
+                    {
+                        code: "document.all[0].innerText",
+                        runAt: "document_start",
+                    },
+                    function (results) {
+                        if (results[0].length == 0) {
+                            webSocket.send("pdf:" + url);
+                        } else {
+                            webSocket.send("text:start:" + url);
+                            chunkString(results[0], 500).forEach((chunk) => {
+                                webSocket.send(chunk);
+                            });
+                            webSocket.send("text:end");
+                        }
+                    },
+                );
             },
         );
-    });
-};
+    };
 
-webSocket.onclose = (event) => {
-    console.log("websocket closed");
-    webSocket = null;
-};
+    webSocket.onclose = (event) => {
+        console.log("websocket closed");
+
+        running = false;
+    };
+
+    webSocket.onerror = (event) => {
+        console.log("websocket error");
+
+        running = false;
+    };
+}
+
+setInterval(create_web_socket, 1000);
