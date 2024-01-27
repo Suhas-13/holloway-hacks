8d7bdb125c75f4030c22369cc07ea184cd354cb0
Ollie Stone <ollieandy.stone@gmail.com>
2024-01-27 13:04:13 +0000

    add support for pdfs

--- extension/background.js
+++ extension/background.js
@@ -2,33 +2,22 @@ let webSocket = new WebSocket("ws://localhost:8001");
 
 webSocket.onopen = (event) => {
     console.log("websocket open");
-    webSocket.send("Hello from the client!");
-
-    // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    //     console.log("yoo");
-    //     console.log(tab);
-    //     // tab.executeScript(
-    //     //     null,
-    //     //     {
-    //     //         code: "document.all[0].innerText",
-    //     //         runAt: "document_start",
-    //     //     },
-    //     //     function (results) {
-    //     //         webSocket.send(results[0]);
-    //     //     },
-    //     // );
-    // });
 };
 
 webSocket.onmessage = (event) => {
-    console.log(`websocket received message: ${event.data}`);
-
+    console.log("data requested");
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         if (tabs.length == 0) {
             return;
         }
 
         let id = tabs[0].id;
+        let url = tabs[0].url;
+
+        if (url.startsWith("chrome://")) {
+            webSocket.send("");
+            return;
+        }
 
         chrome.tabs.executeScript(
             id,
@@ -37,14 +26,21 @@ webSocket.onmessage = (event) => {
                 runAt: "document_start",
             },
             function (results) {
-                console.log(results);
-                webSocket.send(results[0]);
+                if (results[0].length == 0) {
+                    fetch(url)
+                        .then((response) => response.text())
+                        .then((data) => {
+                            webSocket.send(data);
+                        });
+                } else {
+                    webSocket.send(results[0]);
+                }
             },
         );
     });
 };
 
 webSocket.onclose = (event) => {
-    console.log("websocket connection closed");
+    console.log("websocket closed");
     webSocket = null;
 };
