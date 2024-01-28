a0856e51cd3d25879ee8fc97f81f1a430e3c89cc
Suhas Hariharan <hariharansuhas@gmail.com>
2024-01-28 11:54:03 +0000

    taipy

--- vectors/wip_front.py
+++ vectors/wip_front.py
@@ -1,67 +1,50 @@
-from taipy.gui import Gui, Markdown
+from taipy.gui import Gui, State, Markdown
 from redis_handler import RedisManager
-import pandas as pd
 
 # Initialize RedisManager
 manager = RedisManager()
 
-# Define the navigation bar and page layout
-# nav_bar = """
-# <div style='background-color: #333; padding: 10px; color: white; text-align: center;'>
-#     <h3>My Website</h3>
-# </div>
-# """
-nav_bar = ""
+# Initial state
+State.file_urls = ["https://www.google.com", "https://www.google.com"]
+State.user_input = ""
+State.text = "Hello! How can I assist you today?"
 
-page = nav_bar + """
+class User:
+    def __init__(self, id, name, birth_year):
+        self.id, self.name, self.birth_year = (id, name, birth_year)
+
+users = [
+    User(231, "Johanna", 1987),
+
+    ]
+
+user_sel = users[0]
+
+# Define Markdown page with dynamic file section using Taipy bindings
+page = """
 <|layout|type=flexbox|orientation=horizontal|>
     <h1 class="head1">SecondBrain Q&A</h1>
         <center>
-            <|{user_input}|input|placeholder=Enter your question here...|>
+            <|{State.user_input}|input|placeholder=Enter your question here...|>
             <|Ask|button|on_action=on_button_action|>
         </center>
-            <|{text}|>
+            <|{State.text}|
 
 </>
 <|SHOW FILES|expandable|
-<section id="file-cont">
-"""
-
-files = ["http://www.example.com/",
-    "https://www.example.com/belief.php",
-    "http://example.com/?bird=airport&birds=adjustment",
-    "http://www.example.com/beginner.php",
-    "https://example.net/",
-    "http://www.example.com/"
-         ]
-
-for file in files:
-    page += f"<a class='file' href='{file}'>{file}</a>"
-
-
-page += """
-</section>
+<|{user_sel}|selector|lov={users}|type=User|adapter={lambda u: (u.id, u.name)}|>
 |>
 """
 
-# Initial state
-text = "Hello! How can I assist you today?"
-user_input = ""
-
 # Action handler for the button
-def on_button_action(state):
-    try:
-        state.text = "Loading..."
-        query = state.user_input
-        response = manager.ask_gpt(query)
-        state.text = response
-    except Exception as e:
-        state.text = f"Error: {str(e)}"
-    finally:
-        state.user_input = ""
+def on_button_action():
+    print("Button clicked")
+    users.append(User(1, "John", 1979))
+    State.file_urls.append("https://abc.com")
 
 # Linking the CSS file
 css_file = "styles.css"
 
-# Run the GUI
-Gui(page=page, css_file=css_file).run(port=3001)
+# Run the GUI with the Markdown page
+gui = Gui(page=Markdown(page), css_file=css_file)
+gui.run(port=3001)
